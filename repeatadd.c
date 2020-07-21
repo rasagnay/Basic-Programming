28ca34ecf6fe7f16e3a6a3f2f503c148a2ded87f
rasagnay <63460932+rasagnay@users.noreply.github.com>
2020-07-21 22:26:15 +0530

    Create repeatadd.c

---
+++ repeatadd.c
@@ -0,0 +1,32 @@
+// PROBLEM //
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+// CODE //
+#include<stdio.h> 
+int main(void) 
+{ 
+    long num; 
+    int dig,sum,t,i; 
+   scanf("%d",&t);
+   for(i=0;i<t;i++)
+   
+   {
+    scanf("%ld",&num); 
+   
+    do 
+    { 
+        sum = 0;  
+        while(num!=0)  
+        { 
+            dig=num%10; 
+            num/=10; 
+            sum=sum+dig; 
+            
+        } 
+       
+        num=sum; 
+    }while(num/10!=0); 
+     printf("%d\n",sum); 
+   }
+    return 0; 
+}
