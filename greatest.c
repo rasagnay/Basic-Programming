05fe18e57a01ea4174e0f765d4f1b17767c145ae
rasagnay <63460932+rasagnay@users.noreply.github.com>
2020-07-21 22:42:19 +0530

    Create greatest.c

---
+++ greatest.c
@@ -0,0 +1,25 @@
+// PROBLEM //
+Write a program to find the greatest of the three numbers. 
+
+// CODE //
+
+#include <stdio.h>
+int main() {
+int t,a,b,c,i;
+scanf("%d",&t);
+for(i=0;i<t;i++)
+{
+scanf("%d%d%d",&a,&b,&c);
+   
+    if(a>b && a>c)
+    printf("%d\n",a);
+    else if(b>a && b>c)
+    printf("%d\n",b);
+    else 
+    printf("%d\n",c);
+   
+    
+    
+}
+return 0;
+}
