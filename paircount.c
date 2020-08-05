0cf17e404739b8df703ef037b30c472d49c7c5a2
rasagnay <63460932+rasagnay@users.noreply.github.com>
2020-08-05 17:50:44 +0530

    Update paircount.c

--- paircount.c
+++ paircount.c
@@ -5,30 +5,34 @@ But you ask salesman that you will buy only those articles which are actually in
 In this store, articles are referred as integers. So you have to tell total no. of articles you will buy for your pets.
 
 // CODE //
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int t;
-	cin>>t;
-	while(t--) {
-	    int n,count=0;
-	    cin>>n;
-	    vector<int> v(n);
-	    map<int,int> m;
-	    for(int i=0; i<n; i++) {
-	        cin>>v[i];
-	        m[v[i]]++;
-	        if(m[v[i]] % 2 == 0) {
-	           count ++;
-	        }
-	    }
-	    
-	    cout<<2*count<<endl;
-	}
-	return 0;
+  int t,i;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    cin>>arr[i];
+    sort(arr,arr+n);
+    int count=0;
+    int i=0;
+    while(i<n)
+    {
+      if(arr[i]==arr[i+1])
+      {
+        count++;
+        i=i+2;
+      }
+      else
+      i++;
+
+    }
+    cout<<(count*2);
+    cout<<endl;
+  }
+  return 0;
 }
